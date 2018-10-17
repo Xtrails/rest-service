566426ccc3144a87f1e90a1c9d14b72c41740063
Pavel Anisimov <anisimov.pa@udmonline.ru>
2018-10-17 17:55:25 +0400

    Spring Boot REST: подключаем базу данных, настраиваем Jackson

---
+++ src/main/java/letscode/restservice/domain/Views.java
@@ -0,0 +1,10 @@
+package letscode.restservice.domain;
+
+public final class Views {
+    //TODO: разобраться с наследованием интерфейсов и классо
+    public interface Id{ }
+
+    public interface IdName extends Id{}
+
+    public interface FullMessage extends Id{}
+}
---
+++ src/main/java/letscode/restservice/repo/MessageRepo.java
@@ -0,0 +1,8 @@
+package letscode.restservice.repo;
+
+import letscode.restservice.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
+
+}
